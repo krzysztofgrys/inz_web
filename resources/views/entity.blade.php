a85cd067dd91a68422d64e691b52d1b2015378d9
krzysztof <sirpirato@gmail.com>
2017-12-18 22:57:45 +0100

    improve comments

--- resources/views/entity.blade.php
+++ resources/views/entity.blade.php
@@ -25,50 +25,50 @@
                         </div>
                     @endif
 
-                        <div class="entity">
-                            <div class="entity-left">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="circle"><i class="fa fa-btc"></i>{{ $data->rating }}</div>
-                                    </div>
-                                    <a href="{{ $data->url}}">
-                                        <div class="col-md-4">
-                                            <img class="media-object"
-                                                 src="{{ asset('storage/image/entity/'.$data->thumbnail) }}"
-                                                 alt="Kurt">
-                                        </div>
-                                    </a>
+                    <div class="entity">
+                        <div class="entity-left">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="circle"><i class="fa fa-btc"></i>{{ $data->rating }}</div>
                                 </div>
-                            </div>
-
-                            <div class="entity-right">
-                                <a href="{{  $data->url}}">
-                                    <div class="entity-title">{{ $data->title }}
+                                <a href="{{ $data->url}}">
+                                    <div class="col-md-4">
+                                        <img class="media-object"
+                                             src="{{ asset('storage/image/entity/'.$data->thumbnail) }}"
+                                             alt="Kurt">
                                     </div>
                                 </a>
-                                <div class="entity-description">
-                                    <a href="{{ $data->url}}">
-                                        {{ $data->description }}
-                                    </a>
-                                    {{--<div class="entity-info">--}}
-                                    <div class="entity-info-left">
-                                        <a href="{{  '/profile/' . $data->user_id }}">
-                                            <i class="fa fa-user"> dodał: {{ '@'.$data->user_name }}</i>
-                                        </a>
-                                        <strong> | </strong>
-                                        <i class="fa fa-calendar-check-o"> {{ $data->created_at }}</i>
-
-                                        <strong> | </strong>
-                                        <a href="{{  $data->url }}">
-                                            <i class="fa fa-globe"> domena:
-                                                {{ $data->domain }}</i>
-                                        </a>
-                                    </div>
-                                    {{--</div>--}}
-                                </div>
                             </div>
-
                         </div>
+
+                        <div class="entity-right">
+                            <a href="{{  $data->url}}">
+                                <div class="entity-title">{{ $data->title }}
+                                </div>
+                            </a>
+                            <div class="entity-description">
+                                <a href="{{ $data->url}}">
+                                    {{ $data->description }}
+                                </a>
+                                {{--<div class="entity-info">--}}
+                                <div class="entity-info-left">
+                                    <a href="{{  '/profile/' . $data->user_id }}">
+                                        <i class="fa fa-user"> dodał: {{ '@'.$data->user_name }}</i>
+                                    </a>
+                                    <strong> | </strong>
+                                    <i class="fa fa-calendar-check-o"> {{ $data->created_at }}</i>
+
+                                    <strong> | </strong>
+                                    <a href="{{  $data->url }}">
+                                        <i class="fa fa-globe"> domena:
+                                            {{ $data->domain }}</i>
+                                    </a>
+                                </div>
+                                {{--</div>--}}
+                            </div>
+                        </div>
+
+                    </div>
 
 
                 </div>
@@ -88,6 +88,7 @@
                 @endif
 
                 @foreach($comments as $comment)
+                    {{--{{ dd($comment) }}--}}
                     <div class="entity">
                         <div class="entity-left">
                             <div class="row">
@@ -100,19 +101,21 @@
                         </div>
 
                         <div class="entity-right">
-                            <div class="entity-title">{{ $comment->comments }}
-                            </div>
-                            <div class="entity-description">
+                            <div class="comment-info">
+                                <div class="comment-info-text">
+                                    Dodał: <a href=" {{ '/profile/'.$comment->user_id }}  ">{{ $comment->name }}</a><strong> | </strong> {{ $comment->created_at }}
 
-                                @auth()
-                                    @if ($comment->user_id == Auth::user()->user->user->id)
+
                                         <div class="text-right pull-right">
-                                            <a href="/entity/{{ $data->id }}/delete" class="btn btn-danger btn-xs">Usuń</a>
+                                            <a href="/entity/{{ $data->id }}/comment/edit" class="btn btn-info btn-xs">Edytuj</a>
+                                            <a href="/entity/{{ $data->id }}/comment/delete" class="btn btn-danger btn-xs">Usuń</a>
 
-                                        </div>)
+                                        </div>
+                                </div>
 
-                                    @endif
-                                @endauth
+                            </div>
+                            <div class="comment-body">
+                                {{ $comment->comments }}
                             </div>
                         </div>
                     </div>
@@ -130,14 +133,13 @@
                 <form class="form-horizontal" method="POST" action="{{ route('addComment', $data->id ) }}">
                     {{ csrf_field() }}
 
-
                     <div class="form-group{{ $errors->has('comment') ? ' has-error' : '' }}">
                         <label for="body" class="col-md-2 control-label"><img class="media-object"
                                                                               src="https://media.licdn.com/mpr/mpr/shrinknp_100_100/AAEAAQAAAAAAAAofAAAAJDVjNmI4NzcwLTA2NTktNDZhNS04MWNhLThkNWIwNGJkNGQyNw.png"
                                                                               alt="Kurt"></label>
 
                         <div class="col-md-8">
-                            <textarea id="comment" type="text" class="form-control" name="comment" value="{{ old('comment') }}"></textarea>
+                            <textarea id="comment" type="text" class="form-control" name="comment" value="{{ old('comment') }}" required></textarea>
                             {{--<input id="receiver" name="receiver" type="hidden" value="{{ $receiver->name }}">--}}
 
                             @if ($errors->has('comment'))
@@ -147,7 +149,6 @@
                             @endif
                         </div>
                     </div>
-
 
                     <div class="form-group">
                         <div class="col-md-12 col-md-offset-9">
